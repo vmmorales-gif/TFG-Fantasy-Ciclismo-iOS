6697e100b3019f3271dd58d384e6b22a2b455a39
vmmorales-gif <vmmorales@alu.ucam.edu>
2025-12-25 10:56:25 +0100

    Add files via upload

---
+++ FantasyCiclismov1/FantasyCiclismo/Controladores/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FantasyCiclismo
+//
+//  Created by Victor Morales Valencia on 15/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FantasyCiclismov1/FantasyCiclismo/Controladores/ViewController_Login.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController_Login.swift
+//  FantasyCiclismo
+//
+//  Created by Victor Morales Valencia on 15/11/25.
+//
+
+import UIKit
+
+class ViewController_Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
